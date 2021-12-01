be3dc7bb10854278c0e60c3ce49dc94cc8a6b23c
Ghyunghee Jang <Ghyunghee@gmail.com>
2021-12-01 12:05:37 +0900

    changed create_splits.py

--- create_splits.py
+++ create_splits.py
@@ -6,7 +6,7 @@ import random
 import numpy as np
 
 from utils import get_module_logger
-
+import shutil
 
 def split(source, destination):
     """
@@ -18,7 +18,24 @@ def split(source, destination):
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
     # TODO: Implement function
+    files = [filename for filename in glob.glob(f'{source}/*.tfrecord')]
+    np.random.shuffle(files)
+    train_files, valid_files, test_files = np.split(files, [int(.75*len(files)), int(.9*len(files))])
 
+    train = os.path.join(destination, 'train')
+    os.makedirs(train,exist_ok=True)
+    for file in train_files:
+        shutil.move(file, train)
+
+    valid = os.path.join(destination, 'valid')
+    os.makedirs(valid,exist_ok=True)
+    for file in valid_files:
+        shutil.move(file, valid)
+
+    test = os.path.join(destination, 'test')
+    os.makedirs(test,exist_ok=True)
+    for file in test_files:
+        shutil.move(file, test)   
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
